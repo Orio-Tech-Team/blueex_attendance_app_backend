3d1fe0cd687123b7132c36463132e8c47e322bff
Muhammad Ismail <itsmuhammadismail@gmail.com>
2022-10-28 10:45:57 +0500

    manual api converted to post

--- src/modules/attendacne/attendacne.controller.ts
+++ src/modules/attendacne/attendacne.controller.ts
@@ -8,44 +8,61 @@ import { GetAttendanceDataDto } from './dto/get-attendance-data.dto';
 
 @Controller('attendance')
 export class AttendacneController {
-    constructor(
-        private readonly attendacneService: AttendacneService,
-        private readonly employeeService: EmployeeService
-    ) {}
+  constructor(
+    private readonly attendacneService: AttendacneService,
+    private readonly employeeService: EmployeeService,
+  ) {}
 
-    @ApiBearerAuth('JWT-auth')
-    @Get()
-    async markAttendance(@Req() request) : Promise<any>{
-        const employeeNumber = request.user_information.refrence_number
-        const employee = await this.employeeService.findByShift(employeeNumber)
-        return await this.attendacneService.markAttendance(employee);
-    }
+  @ApiBearerAuth('JWT-auth')
+  @Get()
+  async markAttendance(@Req() request): Promise<any> {
+    const employeeNumber = request.user_information.refrence_number;
+    const employee = await this.employeeService.findByShift(employeeNumber);
+    return await this.attendacneService.markAttendance(employee);
+  }
 
-    @Get("manual")
-    async markAttendanceManually(@Req() request) : Promise<any>{
-        const employeeNumber = request.body.emp_id;
-        const employee = await this.employeeService.findByShift(employeeNumber);
-        const date: string = request.body.date;
-        const inTime: string = request.body.in_time;
-        const outTime: string = request.body.out_time;
+  @Post('manual')
+  async markAttendanceManually(@Req() request): Promise<any> {
+    const employeeNumber = request.body.emp_id;
+    const employee = await this.employeeService.findByShift(employeeNumber);
+    const date: string = request.body.date;
+    const inTime: string = request.body.in_time;
+    const outTime: string = request.body.out_time;
 
-        return await this.attendacneService.markAttendanceManually(employee, date, inTime, outTime);
-    }
+    return await this.attendacneService.markAttendanceManually(
+      employee,
+      date,
+      inTime,
+      outTime,
+    );
+  }
 
-    @ApiBearerAuth('JWT-auth')
-    @Post()
-    async getAttendance(@Body() getAttendanceDto : GetAttendanceDto , @Req() request){
-        const employeeNumber = request.user_information.refrence_number
-        return await this.attendacneService.getAttendanceByMonth(getAttendanceDto,employeeNumber)
-    }
+  @ApiBearerAuth('JWT-auth')
+  @Post()
+  async getAttendance(
+    @Body() getAttendanceDto: GetAttendanceDto,
+    @Req() request,
+  ) {
+    const employeeNumber = request.user_information.refrence_number;
+    return await this.attendacneService.getAttendanceByMonth(
+      getAttendanceDto,
+      employeeNumber,
+    );
+  }
 
-    @Post("server")
-    async getAttendanceServer(@Body() getAttenadanceServerData : GetAttendanceServerDto){
-        return await this.attendacneService.getAttendanceServerUpdate(getAttenadanceServerData)
-    }
+  @Post('server')
+  async getAttendanceServer(
+    @Body() getAttenadanceServerData: GetAttendanceServerDto,
+  ) {
+    return await this.attendacneService.getAttendanceServerUpdate(
+      getAttenadanceServerData,
+    );
+  }
 
-    @Post("getattendancedata")
-    async function (@Body() getAttendanceDataDto : GetAttendanceDataDto) {
-        return await this.attendacneService.getAttendanceDataDto(getAttendanceDataDto)
-    }
+  @Post('getattendancedata')
+  async function(@Body() getAttendanceDataDto: GetAttendanceDataDto) {
+    return await this.attendacneService.getAttendanceDataDto(
+      getAttendanceDataDto,
+    );
+  }
 }
