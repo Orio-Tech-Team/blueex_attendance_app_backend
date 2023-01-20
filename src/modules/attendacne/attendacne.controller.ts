fb22efee9e6e52e19285cb13d54bdd35b54c892b
Ateeb Khan <ateebkhan997@gmail.com>
2023-01-20 16:36:24 +0500

    Email API Updated

--- src/modules/attendacne/attendacne.controller.ts
+++ src/modules/attendacne/attendacne.controller.ts
@@ -144,12 +144,8 @@ export class AttendacneController {
       `,
     });
     //
-    console.log('Message sent: %s', info.messageId);
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
-    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    //
-    return '';
+    return {
+      message: 'request success!',
+    };
   }
 }
